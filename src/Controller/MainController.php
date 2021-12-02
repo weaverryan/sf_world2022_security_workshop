f9a1f74b9c8d6212bc96d33e854153dc81dffcdc
Ryan Weaver <ryan@thatsquality.com>
2021-12-02 10:30:37 -0500

    Adding a login controller and template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,12 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @IsGranted("IS_AUTHENTICATED_FULLY")
      * @Route("/", name="app_homepage")
      */
     public function homepage()
