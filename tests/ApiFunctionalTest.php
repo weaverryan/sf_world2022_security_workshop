f686f866bd6679beff31653d986d0e18ce24d6f2
Ryan Weaver <ryan@thatsquality.com>
2022-06-13 11:11:07 -0400

    Resetting database in the test

--- tests/ApiFunctionalTest.php
+++ tests/ApiFunctionalTest.php
@@ -4,10 +4,12 @@ namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Browser\Test\HasBrowser;
+use Zenstruck\Foundry\Test\ResetDatabase;
 
 class ApiFunctionalTest extends KernelTestCase
 {
     use HasBrowser;
+    use ResetDatabase;
 
     public function testApiProcess(): void
     {
