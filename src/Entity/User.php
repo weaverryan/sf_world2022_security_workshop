8280d1da503c6ef90c8dd60b1d51e199f05d1b86
Ryan Weaver <ryan@thatsquality.com>
2022-06-13 11:54:56 -0400

    fixing bad indentation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,9 +11,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-     #[ORM\Id]
-     #[ORM\GeneratedValue]
-     #[ORM\Column()]
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
