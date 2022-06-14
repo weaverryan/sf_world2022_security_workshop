2c283fd7f09751ded457cb129c3ef4e817635ad8
Ryan Weaver <ryan@thatsquality.com>
2022-06-14 13:01:52 -0400

    Step 41: Listener: Finish logic

--- src/EventListener/LastLoggedInAtSubscriber.php
+++ src/EventListener/LastLoggedInAtSubscriber.php
@@ -2,11 +2,35 @@
 
 namespace App\EventListener;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 class LastLoggedInAtSubscriber implements EventSubscriberInterface
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function onLoginSuccess(LoginSuccessEvent $event)
+    {
+        $user = $event->getUser();
+        if (!$user instanceof User) {
+            throw new \Exception('Invalid user');
+        }
+
+        $user->setLastLoggedInAt(new \DateTimeImmutable('now'));
+        $this->entityManager->flush();
+    }
+
     public static function getSubscribedEvents()
     {
+        return [
+            LoginSuccessEvent::class => 'onLoginSuccess'
+        ];
     }
 }
