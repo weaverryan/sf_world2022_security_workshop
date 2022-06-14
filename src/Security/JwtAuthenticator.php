70df5ce2d9909b5021a3882c6b6e92b54c11123e
Ryan Weaver <ryan@thatsquality.com>
2022-06-14 13:01:47 -0400

    Step 27: JWTAuth: onAuthenticationSuccess()

--- src/Security/JwtAuthenticator.php
+++ src/Security/JwtAuthenticator.php
@@ -57,7 +57,7 @@ class JwtAuthenticator extends AbstractAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        return null;
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
