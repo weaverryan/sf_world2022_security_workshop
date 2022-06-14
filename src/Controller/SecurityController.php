6fb947e8626ba2926f266321ac5bf555a643bc53
Ryan Weaver <ryan@thatsquality.com>
2022-06-14 13:01:50 -0400

    Step 34: 2fa: Remove dd()

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -38,7 +38,6 @@ class SecurityController extends AbstractController
             $entityManager->flush();
         }
 
-        dd($this->getUser());
         return $this->render('security/enable2fa.html.twig');
     }
 
