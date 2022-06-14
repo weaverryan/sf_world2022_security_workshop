53eaef8a44ae3000514aca4aec14f324c6e664de
Ryan Weaver <ryan@thatsquality.com>
2022-06-14 13:01:41 -0400

    Step 9: Authenticator: extend abstract class

--- src/Security/QueryParamsAuthenticator.php
+++ src/Security/QueryParamsAuthenticator.php
@@ -2,8 +2,33 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
-class QueryParamsAuthenticator implements AuthenticatorInterface
+class QueryParamsAuthenticator extends AbstractAuthenticator
 {
+    public function supports(Request $request): ?bool
+    {
+        // TODO: Implement supports() method.
+    }
+
+    public function authenticate(Request $request)
+    {
+        // TODO: Implement authenticate() method.
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // TODO: Implement onAuthenticationSuccess() method.
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        // TODO: Implement onAuthenticationFailure() method.
+    }
 }
