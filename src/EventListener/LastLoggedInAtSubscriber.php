cae91202660dc879c97c2b869543a1385ffe07ed
Ryan Weaver <ryan@thatsquality.com>
2022-06-14 13:01:51 -0400

    Step 40: Listener: Create LastLoggedInAtSubscriber

---
+++ src/EventListener/LastLoggedInAtSubscriber.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LastLoggedInAtSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+    }
+}
